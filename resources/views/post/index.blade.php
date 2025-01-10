f528c41171fe36481c92768b6b4099aae5ce5870
Andrej-Nil <and.kucheroff@yandex.ru>
2025-01-10 12:33:41 +0500

    вывел пост

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,13 +4,30 @@
 
 @section('content')
     <div class="container">
+        <div class="breadcrumbs">
+            <a href="{{route('home')}}" class="breadcrumbs__link">Главная</a>
+            <span class="breadcrumbs__slash">/</span>
+            <a class="breadcrumbs__link">Посты</a>
+        </div>
+
         <h1 class="page-title">Посты</h1>
 
+
+        @if($postList)
         <div class="grid col-4">
             @foreach($postList as $post)
                 @include('post.post-card')
             @endforeach
         </div>
+        @else
+            <div class="page-message">
+                <div class="page-message__inner">
+                    <p class="page-message__title">Ничего нет.</p>
+                    <img class="page-message__img" src="{{asset('img/icon/box.png')}}" alt="">
+                    <p class="page-message__text">Похоже автор еще не добвалил постов.</p>
+                </div>
+            </div>
+        @endif
 
     </div>
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container">
-        <h1 class="page-title">{{$post['title']}}</h1>
+
         <div class="breadcrumbs">
             <a href="{{route('home')}}" class="breadcrumbs__link">Главная</a>
             <span class="breadcrumbs__slash">/</span>
@@ -12,5 +12,26 @@
             <span class="breadcrumbs__slash">/</span>
             <a class="breadcrumbs__link">{{$post['title']}}</a>
         </div>
+
+
+        <div class="post">
+            <h1 class="post__title">{{$post['title']}}</h1>
+            <div class="post-image">
+                <img class="post-image__img" src="{{$post['photo']['url']}}" alt="">
+
+                <div class="post-image-btn">
+                    <span class="post-image-btn__shadow"></span>
+                    <div class="post-image-btn__text">Раскрыть изображение</div>
+                </div>
+
+
+            </div>
+
+            <p class="post__text">{{$post['text']}}</p>
+
+        </div>
+
+
+
     </div>
 @endsection
