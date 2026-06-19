fee0392c61068bf258e0babe96608be36ebd27f5
Andrej-Nil <and.kucheroff@yandex.ru>
2026-06-19 11:45:28 +0500

    правки по сайту

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -14,6 +14,6 @@ class Image extends Model
     ];
 
     public function getUrlAttribute(){
-        return  asset('/storage/' . $this->link);
+        return  asset('app/public/' . $this->link);
     }
 }
