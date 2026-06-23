c32d65bee275917f2cb8e4e757eacd4c1ccbc22d
Andrej-Nil <and.kucheroff@yandex.ru>
2026-06-23 16:10:50 +0500

    Делаю форму обратной связи

--- app/Http/Controllers/Api/CallbackController.php
+++ app/Http/Controllers/Api/CallbackController.php
@@ -6,21 +6,33 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\BaseController;
 use App\Http\Requests\Callback\CallbackRequest;
 use App\Mail\CallbackMail;
+use Carbon\CarbonInterval;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\RateLimiter;
+
 class CallbackController extends BaseController
 {
+
     public function callback(CallbackRequest $request){
-     $request->validated();
+        $key = 'contact-form:' . $request->ip();
+        if (RateLimiter::tooManyAttempts($key, 1)) {
+            $secondsLeft = RateLimiter::availableIn($key);
+            $timeLeft = CarbonInterval::seconds($secondsLeft)->cascade()->forHumans(['short' => true]);
+
+            return  $this->sendError(
+                "С этого IP уже было отправлено сообщение. Попробуйте снова через {$timeLeft}.",
+                [],
+                429
+            );
+
+        }
+
+        RateLimiter::hit($key, 86400);
         Mail::to('and.kucheroff@yandex.ru')->send(new CallbackMail($request->validated()));
         return $this->sendResponse([
             'message' =>  'Ваше письмо отправленно.<br/> В скором времени я с вами свяжусь'
         ]);
 
-
-//     dd($validated);
-//        $validatedData = $request->validate([
-//            'number' => ['required', 'regex:/^([0-9\s\-\+\(\)]*)$/|','min:9', 'min:15'],
-//        ]);
     }
 
 }
